f2a23d2b4dce3b91055094ed7f1708d5bb6102ca
Nina <ninanazarova.fe@pm.me>
2024-03-08 17:19:21 +0100

    refactor(RecentTreatments): lift request logic up to App

--- src/components/RecentTreatments.tsx
+++ src/components/RecentTreatments.tsx
@@ -1,67 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import client from '../api';
 import Treatment from '../models/TreatmentModel';
 import { Box, Card, CardContent, Typography } from '@mui/joy';
+import dayjs from 'dayjs';
 
 type Props = {
-  id?: string | null;
+  treatments: Treatment[] | [];
 };
 
-const RecentTreatments = ({ id }: Props) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [treatments, setTreatments] = useState<Treatment[] | []>([]);
-
-  useEffect(() => {
-    const fetchTreats = async () => {
-      try {
-        const treatments = await client.getTreatments();
-        setTreatments(treatments);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchTreats();
-  }, []);
-
-  useEffect(() => {
-    const fetchTreat = async (id: string) => {
-      try {
-        setIsLoading(true);
-        const treatment = await client.getTreatment(id);
-        if (treatment !== null) {
-          setTreatments((trts) => [treatment, ...trts]);
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (typeof id === 'string') {
-      fetchTreat(id);
-    }
-  }, [id]);
-
+const RecentTreatments = ({ treatments }: Props) => {
   let content;
   if (treatments.length === 0) {
     content = <span>There is no recent treatments</span>;
   } else {
-    content = (treatments as Treatment[]).map((treat: Treatment) => {
+    content = (treatments as Treatment[]).map((treat) => {
       return (
         <Card key={treat.identifier} variant='soft' sx={{ mt: 1 }}>
           <CardContent>
             <Typography level='title-md'>{treat.eventType}</Typography>
             <Typography level='body-md'>{treat.notes ? treat.notes : 'No notes'}</Typography>
+            <Typography level='body-md'>{dayjs(treat.date).format('HH:mm')}</Typography>
           </CardContent>
         </Card>
       );
     });
   }
 
-  return <Box sx={{ mx: 4 }}>{isLoading ? 'Loading...' : content}</Box>;
+  return <Box sx={{ mx: 4 }}>{content}</Box>;
 };
 
 export default RecentTreatments;
