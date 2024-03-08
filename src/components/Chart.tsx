6f62cb4f37de41b79aceabf48cbe5fa12f6ab923
Nina <ninanazarova.fe@pm.me>
2024-03-08 17:05:42 +0100

    feat(Chart): add treatments points

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,6 @@
 import Entry from '../models/EntryModel';
+import Treatment from '../models/TreatmentModel';
+
 import dayjs from 'dayjs';
 import {
   ChartsAxisHighlight,
@@ -7,14 +9,12 @@ import {
   ChartsXAxis,
   ChartsYAxis,
   LinePlot,
+  MarkPlot,
   ResponsiveChartContainer,
   useDrawingArea,
   useYScale,
 } from '@mui/x-charts';
-
-type Props = {
-  dataset: Entry[];
-};
+import { useEffect, useState } from 'react';
 
 const NormalRange = () => {
   const low = 5;
@@ -27,26 +27,85 @@ const NormalRange = () => {
   return <rect x={left} y={highY} width={width} height={lowY - highY} fill='#f3f9eb' />;
 };
 
-const Chart = ({ dataset }: Props) => {
+interface Record {
+  date: number;
+  isTreatment?: boolean;
+  mbg: number | null;
+}
+const merge = (es: Entry[], ts: Treatment[]): Array<Record> => {
+  let res = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < es.length && j < ts.length) {
+    if (es[i].date >= ts[j].date) {
+      res.push({ ...ts[j], isTreatment: true, mbg: null });
+      j++;
+    } else {
+      res.push(es[i]);
+      i++;
+    }
+  }
+
+  for (; i < es.length; i++) res.push(es[i]);
+  for (; j < ts.length; j++) res.push({ ...ts[j], isTreatment: true, mbg: null });
+
+  return res;
+};
+
+type Props = {
+  entries: Entry[];
+  treatments: Treatment[];
+};
+const Chart = ({ entries, treatments }: Props) => {
+  const [x, setX] = useState<number[]>([]);
+  const [entryPoints, setEntryPoints] = useState<(number | null)[]>([]);
+  const [treatPoints, setTreatPoints] = useState<(number | null)[]>([]);
+
+  useEffect(() => {
+    const x: number[] = [];
+    const eSerie: Array<null | number> = [];
+    const tSerie: Array<null | number> = [];
+    const makeDataset = () => {
+      const merged = merge(entries, treatments);
+      for (let i = 0; i < merged.length; i++) {
+        if (merged[i].isTreatment) {
+          x.push(merged[i].date);
+          // TODO: check which mbg value to push i+1 or i-1 and check if not null
+          tSerie.push(merged[i + 1].mbg);
+          eSerie.push(merged[i + 1].mbg);
+        } else {
+          x.push(merged[i].date);
+          tSerie.push(null);
+          eSerie.push(merged[i].mbg);
+        }
+      }
+    };
+    makeDataset();
+
+    setX(x);
+    setEntryPoints(eSerie);
+    setTreatPoints(tSerie);
+  }, [entries, treatments]);
+
   return (
     <ResponsiveChartContainer
-      dataset={dataset}
       height={400}
       margin={{ top: 20, bottom: 30, left: 30, right: 30 }}
       series={[
+        { type: 'line', data: entryPoints, color: 'black', showMark: false },
         {
-          id: 'main',
           type: 'line',
-          dataKey: 'mbg',
-          showMark: false,
-          color: 'black',
-          valueFormatter: (v: number) => v.toString(),
+          data: treatPoints,
+          color: 'red',
+          showMark: true,
         },
       ]}
       xAxis={[
         {
           id: 'datetime',
-          dataKey: 'date',
+          data: x,
           scaleType: 'time',
           valueFormatter: (v) => dayjs(v).format('HH:mm'),
         },
@@ -54,8 +113,8 @@ const Chart = ({ dataset }: Props) => {
       yAxis={[
         {
           id: 'glucose',
-          min: 0,
-          tickMinStep: 3,
+          min: 3,
+          max: 21,
           valueFormatter: (v: number) => v.toString(),
         },
       ]}
@@ -66,9 +125,11 @@ const Chart = ({ dataset }: Props) => {
       <ChartsReferenceLine y={5} lineStyle={{ stroke: '#cecece' }} />
 
       <NormalRange />
-      <LinePlot id='main' />
+      <LinePlot />
+      <MarkPlot />
+
       <ChartsAxisHighlight x='line' />
-      <ChartsTooltip />
+      <ChartsTooltip trigger='axis' />
 
       <ChartsXAxis axisId='datetime' />
       <ChartsYAxis axisId='glucose' disableLine={true} disableTicks={true} />
