fd20f6e51f1388c331c76b826906e4d90f87ba22
Nina <ninanazarova.fe@pm.me>
2024-04-04 14:43:21 +0200

    feat: make login page fields required

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,20 +1,30 @@
-import { Button, FormControl, FormLabel, Input, Stack, Typography, inputClasses } from '@mui/joy';
-import { useState } from 'react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Typography,
+  inputClasses,
+} from '@mui/joy';
+import { ChangeEvent, useState } from 'react';
 import { useAuth } from '../SetupContext';
 import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  const [token, setToken] = useState('');
   const [url, setUrl] = useState('');
+  const [token, setToken] = useState('');
   const auth = useAuth();
   const navigate = useNavigate();
 
-  const handleSave = async () => {
-    localStorage.setItem('refresh_token', JSON.stringify(token));
+  const handleSave = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
     localStorage.setItem('base_url', JSON.stringify(url));
+    localStorage.setItem('refresh_token', JSON.stringify(token));
 
-    await auth.loginUser(token, url);
-    navigate('/');
+    auth.loginUser(token, url);
+
+    navigate('/', { replace: true });
   };
 
   return (
@@ -26,29 +36,32 @@ const LoginPage = () => {
       sx={{ px: 4, minHeight: '100vh', bgcolor: 'background.level1' }}
     >
       <Typography level='h3'>Set up your Libra</Typography>
-      <Stack
-        spacing={2}
-        sx={{
-          [`& .${inputClasses.root}`]: {
-            '--Input-focusedThickness': '1px',
-          },
-          [`& .${inputClasses.input}`]: {
-            width: '100%',
-          },
-        }}
-      >
-        <FormControl required size='lg'>
-          <FormLabel>NightScout URL</FormLabel>
-          <Input onChange={(e) => setUrl(e.target.value)} />
-        </FormControl>
-        <FormControl required size='lg'>
-          <FormLabel>Token</FormLabel>
-          <Input onChange={(e) => setToken(e.target.value)} />
-        </FormControl>
-        <Button size='lg' type='button' onClick={handleSave}>
-          Save
-        </Button>
-      </Stack>
+      <form onSubmit={handleSave}>
+        <Stack
+          spacing={2}
+          sx={{
+            [`& .${inputClasses.root}`]: {
+              '--Input-focusedThickness': '1px',
+            },
+            [`& .${inputClasses.input}`]: {
+              width: '100%',
+            },
+          }}
+        >
+          <FormControl size='lg'>
+            <FormLabel>NightScout URL</FormLabel>
+            <Input value={url} onChange={(e) => setUrl(e.target.value)} required />
+          </FormControl>
+          <FormControl size='lg'>
+            <FormLabel>Token</FormLabel>
+            <Input value={token} onChange={(e) => setToken(e.target.value)} required />
+          </FormControl>
+
+          <Button size='lg' type='submit'>
+            Save
+          </Button>
+        </Stack>
+      </form>
     </Stack>
   );
 };
